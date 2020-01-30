e90289e083018326afde73ecc4c134511acdc81c
Maseeng <60015243+Maseeng@users.noreply.github.com>
2020-01-30 11:47:55 -0500

    Update MaseengMasitha_Lab1.Python.py

--- MaseengMasitha_Lab1.Python.py
+++ MaseengMasitha_Lab1.Python.py
@@ -1,5 +1,5 @@
-Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
+#Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+#Type "copyright", "credits" or "license()" for more information.
 >>> #Maseeng Masitha
 #24 January 2020
 #Python 3.4
